277a68934dca7b402540d824c7731e77506c0fbc
Roms <romelyn.ungab@ipfx.com>
2017-04-10 13:54:06 +1200

    Accessing resources from the code behind

---
+++ ResourcesFromCode/ResourcesFromCodeBehind.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ResourcesFromCode
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnClickMe_Click(object sender, RoutedEventArgs e)
+        {
+            LbResult.Items.Add(PanelMain.FindResource("StrPanel").ToString());
+            LbResult.Items.Add(this.FindResource("StrWindow").ToString());
+            LbResult.Items.Add(Application.Current.FindResource("StrApp").ToString());
+        }
+    }
+}
