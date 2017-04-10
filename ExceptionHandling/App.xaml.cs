5eff1fdcab9501c5afe25f71352bbd0a4935b759
Roms <romelyn.ungab@ipfx.com>
2017-04-10 14:25:39 +1200

    ExceoptionHandling

---
+++ ExceptionHandling/App.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Threading;
+
+namespace ExceptionHandling
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        private void Application_UnhandledException(object sender, System.Windows.Threading.DispatcherUnhandledExceptionEventArgs e)
+        {
+            MessageBox.Show("An unhandled exception just occured: " + e.Exception.Message, "Exception Example",
+                MessageBoxButton.OK, MessageBoxImage.Warning);
+            e.Handled = true;
+        }
+    }
+}
