7bdff4828a9329839483aab6b640e7a338cb7563
Roms <romelyn.ungab@ipfx.com>
2017-05-03 08:47:31 +1200

    First Caliburn Sample

---
+++ CaliburnEx/ShellViewModel.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Caliburn.PresentationFramework;
+
+namespace CaliburnEx
+{
+    public class ShellViewModel:PropertyChangedBase
+    {
+        private string _message;
+
+        public string Message
+        {
+            get { return _message; }
+            set
+            {
+                _message = value;
+                NotifyOfPropertyChange(()=> Message);
+                NotifyOfPropertyChange(()=> CanChangeMessage);
+            }
+        }
+
+        private int _pressCount;
+
+        public ShellViewModel()
+        {
+            Message = "Hello World";
+        }
+
+        public void ChangeMessage()
+        {
+            _pressCount++;
+            Message = "Presses = " + _pressCount;
+        }
+
+        public bool CanChangeMessage
+        {
+            get { return _pressCount > 10; }
+        }
+    }
+}
