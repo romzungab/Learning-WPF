169f612bb7827f6247e89d3dc94e518c76b850a2
Roms <romelyn.ungab@ipfx.com>
2017-05-03 10:36:48 +1200

    Basic Calburn Config, Actions and Conventions

---
+++ HelloCaliburn/ViewModels/ShellViewModel.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using Caliburn.PresentationFramework;
+
+namespace HelloCaliburn.ViewModels
+{
+    public class ShellViewModel : PropertyChangedBase
+    {
+        private string _Name;
+        private bool _CanSayHello;
+
+        public string Name
+        {
+            get { return _Name; }
+            set
+            {
+                _Name = value;
+                NotifyOfPropertyChange(()=>Name);
+                NotifyOfPropertyChange(() => CanSayHello);
+            }
+        }
+
+        public bool CanSayHello => !string.IsNullOrEmpty(Name);
+
+        public void SayHello()
+        {
+            MessageBox.Show($"Hello {Name}");
+        }
+    }
+    
+}
