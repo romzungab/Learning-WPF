97cede1f9ac45748645848e60db212607fd2f5f3
Roms <romelyn.ungab@ipfx.com>
2017-05-05 11:32:54 +1200

    Caliburn Windows Manager

---
+++ CaliburnWinManager/AppBootstrapper.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.ComponentModel.Composition.Hosting;
+using System.ComponentModel.Composition.Primitives;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using Caliburn.Micro;
+using CaliburnWinManager.ViewModels;
+
+namespace CaliburnWinManager
+{
+    public class AppBootstrapper : BootstrapperBase
+    {
+        private CompositionContainer container;
+
+        public AppBootstrapper()
+        {
+            Initialize();
+        }
+
+        protected override void OnStartup(object sender, StartupEventArgs e)
+        {
+            DisplayRootViewFor<AppViewModel>();
+        }
+        protected override void Configure()
+        {
+            container = new CompositionContainer(new AggregateCatalog(AssemblySource.Instance.Select(x => new AssemblyCatalog(x)).OfType<ComposablePartCatalog>()));
+
+            CompositionBatch batch = new CompositionBatch();
+
+            batch.AddExportedValue<IEventAggregator>(new EventAggregator());
+            batch.AddExportedValue<IWindowManager>(new MyWindow());
+            batch.AddExportedValue(container);
+
+            container.Compose(batch);
+        }
+
+        protected override object GetInstance(Type serviceType, string key)
+        {
+            string contract = string.IsNullOrEmpty(key) ? AttributedModelServices.GetContractName(serviceType) : key;
+            var exports = container.GetExportedValues<object>(contract);
+
+            if (exports.Any())
+            {
+                return exports.First();
+            }
+
+            throw new Exception(string.Format("Could not locate any instances of contract {0}.", contract));
+        }
+    }
+}
---
+++ CaliburnWinManager/ViewModels/AppViewModel.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.Dynamic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using Caliburn.Micro;
+
+namespace CaliburnWinManager.ViewModels
+{
+    [Export(typeof(AppViewModel))]
+    public class AppViewModel: PropertyChangedBase, IHaveDisplayName
+    {
+        private readonly IWindowManager _window;
+        [ImportingConstructor]
+        public AppViewModel(IWindowManager window)
+        {
+            _window = window;
+        }
+        public string DisplayName { get; set; } = "Caliburn Micro Part 5: The Window Manager";
+
+        public void OpenWindow()
+        {
+            dynamic settings = new ExpandoObject();
+            settings.WindowStartupLocation = WindowStartupLocation.Manual;
+
+            _window.ShowWindow(new AppViewModel(_window), null, settings);
+        }
+    }
+
+    public class MyWindow : WindowManager
+    {
+        protected override Window EnsureWindow(object model, object view, bool isDialog)
+        {
+            Window window = base.EnsureWindow(model, view, isDialog);
+            window.SizeToContent = SizeToContent.Manual;
+            window.Width = 300;
+            window.Height = 400;
+            return window;
+        }
+    }
+}
