54286f4532df0803b6134ef031fdd8bbd67eeb63
Roms <romelyn.ungab@ipfx.com>
2017-05-08 09:34:33 +1200

    Caliburn Conduuctor and Screens

---
+++ CaliburnScreenConductor/AppBootstrapper.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.ComponentModel.Composition.Hosting;
+using System.ComponentModel.Composition.Primitives;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using Caliburn.Micro;
+using CaliburnScreenConductor.ViewModels;
+
+namespace CaliburnScreenConductor
+{
+    public class AppBootstrapper:BootstrapperBase
+    {
+        private CompositionContainer container;
+
+        public AppBootstrapper()
+        {
+            Initialize();
+        }
+
+        protected override void OnStartup(object sender, StartupEventArgs e)
+        {
+            DisplayRootViewFor<AppViewModel>();
+        }
+      
+    }
+}
---
+++ CaliburnScreenConductor/ViewModels/AppViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Caliburn.Micro;
+
+namespace CaliburnScreenConductor.ViewModels
+{
+    public class AppViewModel : Conductor<object>
+    {
+        public void ShowRedScreen()
+        {
+            ActivateItem(new RedViewModel());
+        }
+
+        public void ShowGreenScreen()
+        {
+            ActivateItem(new GreenViewModel());
+        }
+
+        public void ShowBlueScreen()
+        {
+            ActivateItem(new BlueViewModel());
+        }
+
+    }
+}
